3599af81a356d9716dd4a59a975d361298a69c79
Varga György Márk <gyorgy.varga@shiwaforce.com>
2023-06-22 10:56:35 +0200

    main page from the edge

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import PostForm from "@/app/components/PostForm";
 import PostList from "@/app/components/PostList";
 
+export const runtime = 'edge'
+
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-24">
