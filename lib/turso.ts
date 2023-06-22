8e17f7fd5eebedeb2e7ea7e38a987293fa92fc26
Varga György Márk <gyorgy.varga@shiwaforce.com>
2023-06-22 10:32:43 +0200

    turso edge function fix

--- lib/turso.ts
+++ lib/turso.ts
@@ -1,5 +1,5 @@
 import { drizzle } from 'drizzle-orm/libsql';
-import { createClient } from '@libsql/client';
+import { createClient } from '@libsql/client/web'
 
 const client = createClient({ url: process.env.DATABASE_URL as string, authToken: process.env.DATABASE_AUTH_TOKEN as string });
 export const db = drizzle(client);
